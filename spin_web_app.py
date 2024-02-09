26da44566df98833fe1dac64e9ef6136a51a26bf
Kosuri Indu <118645569+kosuri-indu@users.noreply.github.com>
2024-02-09 08:39:29 +0530

    Update spin_web_app.py

--- spin_web_app.py
+++ spin_web_app.py
@@ -16,7 +16,7 @@ try:
     st.header("SPIN - Simple Project Idea Navigator")
     st.subheader("Fill the following to get required Project Idea :bulb:")
 
-    data = pd.read_csv('dataset/project_ideas_dataset.csv')
+    data = pd.read_csv('project_ideas_dataset.csv')
 
     st.subheader(":pushpin: 1. Enter Domain")
     domain_options = data['Domain'].unique()
